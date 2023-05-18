f9c8550a2b5980042a546bb9d401cb0dcb3be538
unknown <apurbahasanj@gmail.com>
2023-05-18 19:49:38 +0600

    banners added in mongodb

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_PASS);
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@believer.igrxpib.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +27,19 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+
+    const bannersCollection = client.db('Figurio').collection('banners')
+
+
+    app.get('/banners', async(req, res) => {
+      const cursor = bannersCollection.find()
+      const result = await cursor.toArray()
+      // console.log(result);
+      res.send(result)
+    })
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
