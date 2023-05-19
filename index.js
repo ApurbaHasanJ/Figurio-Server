7a7b9021d92f837f4e29bbeb0ffae013c7124785
unknown <apurbahasanj@gmail.com>
2023-05-19 18:23:48 +0600

    single category dynamic link added

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@believer.igrxpib.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,49 +26,54 @@ async function run() {
     // await client.connect();
     // Send a ping to confirm a successful connection
 
-    const bannersCollection = client.db('Figurio').collection('banners')
-    const galleriesCollection = client.db('Figurio').collection('gallery')
-    const shopByCategoryCollection = client.db('Figurio').collection('shopByCategory')
+    const bannersCollection = client.db("Figurio").collection("banners");
+    const galleriesCollection = client.db("Figurio").collection("gallery");
+    const shopByCategoryCollection = client
+      .db("Figurio")
+      .collection("shopByCategory");
 
-// get banners for homepage
-    app.get('/banners', async(req, res) => {
-      const cursor = bannersCollection.find()
-      const result = await cursor.toArray()
+    // get banners for homepage
+    app.get("/banners", async (req, res) => {
+      const cursor = bannersCollection.find();
+      const result = await cursor.toArray();
       // console.log(result);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // get galleries
-    app.get('/galleries',async(req, res) => {
-      const cursor = galleriesCollection.find()
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+    app.get("/galleries", async (req, res) => {
+      const cursor = galleriesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // get short by categories data
-    app.get('/categories', async(req, res) => {
-      const cursor = shopByCategoryCollection.find()
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+    app.get("/categories", async (req, res) => {
+      const cursor = shopByCategoryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    // get data for single category
+    app.get("/categories/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await shopByCategoryCollection.findOne(query);
+      res.send(result);
+    });
 
-
+   
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
-
-
-
 
 app.get("/", (req, res) => {
   res.send("Figurio is running");
