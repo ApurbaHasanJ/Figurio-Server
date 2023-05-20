0bd338a2bc8b8609474f9da6636087fab3402769
unknown <apurbahasanj@gmail.com>
2023-05-20 16:47:43 +0600

    toy delete url updated

--- index.js
+++ index.js
@@ -149,6 +149,15 @@ async function run() {
       res.send(result);
     });
 
+
+    // delete single toy
+    app.delete('/my-toy/:id',async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await allToysCollection.deleteOne(query)
+      res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
