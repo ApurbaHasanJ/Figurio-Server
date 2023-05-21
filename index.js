5627c01d83424cc86bfe89551b8082f211a33a8f
unknown <apurbahasanj@gmail.com>
2023-05-21 20:34:39 +0600

    updated

--- index.js
+++ index.js
@@ -118,15 +118,14 @@ async function run() {
       const cursor = allToysCollection.find().limit(20);
       const result = await cursor.toArray();
       res.send(result);
-    }); 
-    
+    });
+
     // Latest Products
     app.get("/latest-toys", async (req, res) => {
-      const cursor = allToysCollection.find().sort({ _id: -1 }).limit(10);
+      const cursor = allToysCollection.find().sort({ _id: -1 }).limit(9);
       const result = await cursor.toArray();
       res.send(result);
     });
-    
 
     // creating index on fields
     const indexKeys = { toyName: 1, toySubCategory: 1 };
@@ -148,39 +147,64 @@ async function run() {
       res.send(result);
     });
 
+    
     // get my toys
+    // app.get("/my-toys/:email", async (req, res) => {
+    //   // console.log(req.params.email);
+    //   const result = await allToysCollection
+    //     .find({ sellerEmail: req.params.email })
+    //     .toArray();
+    //   res.send(result);
+    // });
+
+
+    // get my toys with sorting
     app.get("/my-toys/:email", async (req, res) => {
-      // console.log(req.params.email);
-      const result = await allToysCollection
-        .find({ sellerEmail: req.params.email })
-        .toArray();
+      const { email } = req.params;
+
+      try {
+        let toys = await allToysCollection.find({ sellerEmail: email });
+
+        const sortBy = req.query.sortBy;
+        let sortOrder = 1;
+        if (sortBy === "desc") {
+          toys = toys.sort({ toyPrice: -1 });
+          sortOrder = -1;
+        } else {
+          toys = toys.sort({ toyPrice: 1 });
+        }
+        toys = await toys.toArray();
+
+        res.send(toys);
+      } catch (error) {
+        console.error("Error retrieving toys:", error);
+        res.status(500).send("An error occurred while retrieving toys.");
+      }
+    });
+
+    // delete single toy
+    app.delete("/my-toy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allToysCollection.deleteOne(query);
       res.send(result);
     });
 
-
-    // delete single toy
-    app.delete('/my-toy/:id',async(req, res)=>{
+    // update toy info
+    app.put("/my-toy/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await allToysCollection.deleteOne(query)
-      res.send(result)
-    })
-
-// update toy info
-app.put('/my-toy/:id', async (req, res) => {
-  const id = req.params.id
-  const body = req.body;
-  const filter = {_id: new ObjectId(id)}
-  const updateDoc = {
-    $set: {
-      toyPrice: body.toyPrice,
-      toyQuantity: body.toyQuantity,
-      toyDetails: body.toyDetails
-    }
-  }
-  const result = await allToysCollection.updateOne(filter, updateDoc)
-  res.send(result)
-})
+      const body = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          toyPrice: body.toyPrice,
+          toyQuantity: body.toyQuantity,
+          toyDetails: body.toyDetails,
+        },
+      };
+      const result = await allToysCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
